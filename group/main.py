a22a70484a58f526acfef5c4231583c7d2089761
Okonicha Chukwudinma <ddunebari@gmail.com>
2024-06-24 10:47:47 +0700

    key generation

--- group/main.py
+++ group/main.py
@@ -1,22 +1,24 @@
 import os
 import json
-import ecdsa
-from hashlib import sha256
+from nacl.signing import SigningKey
+from nacl.encoding import HexEncoder
+from nacl.hash import sha256
 
+# using libsodium for key generation
 def generate_key_pair():
-    sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
-    vk = sk.get_verifying_key()
+    sk = SigningKey.generate()
+    vk = sk.verify_key
     return sk, vk
 
 def create_dummy_transaction(sk):
     nonce = os.urandom(16).hex()
     message = nonce.encode()
-    signature = sk.sign(message)
-    vk = sk.get_verifying_key()
+    signature = sk.sign(message, encoder=HexEncoder).signature.decode()
+    vk = sk.verify_key.encode(encoder=HexEncoder).decode()
     transaction = {
         'nonce': nonce,
-        'signature': signature.hex(),
-        'public_key': vk.to_string().hex()
+        'signature': signature,
+        'public_key': vk
     }
     return json.dumps(transaction)
 
